0843f8c138148cdf0635c0cadacc222929244582
Megiolaro <giancarlosmegiolaro@gmail.com>
2023-09-20 14:29:42 -0300

    Função de adicionar no carrinho

--- script.js
+++ script.js
@@ -1,84 +1,10 @@
+import { renderizarCatalogo } from "./src/cartaoProduto"
 import { carrinho } from "./src/menuCarrinho"
 
-const catalogo = [
-    {
-        id: 1,
-        nome: "Camisa Larga com Bolsos",
-        marca: "Zara",
-        preco: 70,
-        nomeArquivoImagem: "product-1.jpg",
-        feminino: false
-    },
-    {
-        id: 2,
-        nome: "Casaco Reto com Lã",
-        marca: "Zara",
-        preco: 85,
-        nomeArquivoImagem: "product-2.jpg",
-        feminino: true
-    },
-    {
-        id: 3,
-        nome: "Jaqueta com Efeito Camurça",
-        marca: "Zara",
-        preco: 60,
-        nomeArquivoImagem: "product-3.jpg",
-        feminino: false
-    },
-    {
-        id: 4,
-        nome: "Sobretudo em Mescla de Lã",
-        marca: "Zara",
-        preco: 160,
-        nomeArquivoImagem: "product-4.jpg",
-        feminino: false
-    },
-    {
-        id: 5,
-        nome: "Camisa Larga Acolchoada de Veludo Cotelê",
-        marca: "Zara",
-        preco: 110,
-        nomeArquivoImagem: "product-5.jpg",
-        feminino: false
-    },
-    {
-        id: 6,
-        nome: "Casaco de Lã com Botôes",
-        marca: "Zara",
-        preco: 170,
-        nomeArquivoImagem: "product-6.jpg",
-        feminino: true
-    },
-    {
-        id: 7,
-        nome: "Casaco com Botôes",
-        marca: "Zara",
-        preco: 75,
-        nomeArquivoImagem: "product-7.jpg",
-        feminino: true
-    },
-    {
-        id: 8,
-        nome: "Colete Comprido com Cinto",
-        marca: "Zara",
-        preco: 88,
-        nomeArquivoImagem: "product-8.jpg",
-        feminino: true
-    }
-]
 
-for (const produtoCatalogo of catalogo) {
-    const cartaoProduto = `<div class="border-solid border-2 border-sky-500 m-2 w-48 id="produtc-${produtoCatalogo.id}">
-    <img src="./assets/img/${produtoCatalogo.nomeArquivoImagem}" alt="imagem1" style="height: 300px;">
-    <p>${produtoCatalogo.marca}</p>
-    <p>${produtoCatalogo.nome}</p>
-    <p>$${produtoCatalogo.preco}</p>
-    <button>Adicionar</button>
-    </div>`
 
-    document.getElementById("container").innerHTML += cartaoProduto
-}
 
+renderizarCatalogo()
 carrinho()
 
 
--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades"
+
 function abrirCarrinho(){
     document.getElementById("carrinho").classList.remove("right-[-360px]")
     document.getElementById("carrinho").classList.add("right-0")
@@ -14,4 +16,19 @@ export function carrinho(){
 
     botaoAbreCarrinho.addEventListener("click", abrirCarrinho)
     botaoFechaCarrinho.addEventListener("click", fecharCarrinho)
+}
+
+export function adicionarAoCarrinho(idProduto){
+    const produto = catalogo.find((p) => p.id === idProduto)
+    const containerProdutoCarrinho = document.getElementById("produtos-carrinho")
+    const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative m-2">
+    <button id="botao-fecha-carrinho"><i class="fa-solid fa-circle-xmark text-slate-800 absolute top-1 right-1 hover:text-slate-400"></i></button>
+    <img src="assets/img/${produto.nomeArquivoImagem}" alt="produto 1" class="h-24 rounded-lg">
+    <div class="py-2">
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
+      <p class="text-slate-400 text-xs">Tamanho: M</p>
+      <p class="text-green-700 text-lg">$ ${produto.preco}</p>
+    </div>
+  </article>`
+  containerProdutoCarrinho.innerHTML += cartaoProdutoCarrinho
 }
